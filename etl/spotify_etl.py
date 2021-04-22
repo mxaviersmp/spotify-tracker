8d203227874b0b5627844308e1b67509d6cc3a7d
flych3r <matheus.sampaio011@gmail.com>
2021-04-22 17:47:26 -0300

    :card_file_box: updated etl

---
+++ etl/spotify_etl.py
@@ -0,0 +1,56 @@
+import os
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+from sptf.spotify_tasks import (
+    run_get_artist_info,
+    run_get_played_tracks,
+    run_get_track_info,
+    run_update_access_tokens,
+)
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': [os.getenv('EMAIL')],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'spotify_etl',
+    default_args=default_args,
+    description='DAG to update db',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(2),
+    tags=['spotify'],
+)
+
+t1 = PythonOperator(
+    task_id='update_access_tokens',
+    python_callable=run_update_access_tokens,
+    dag=dag
+)
+
+t2 = PythonOperator(
+    task_id='get_played_tracks',
+    python_callable=run_get_played_tracks,
+    dag=dag
+)
+t3 = PythonOperator(
+    task_id='get_track_info',
+    python_callable=run_get_track_info,
+    dag=dag
+)
+t4 = PythonOperator(
+    task_id='get_artist_info',
+    python_callable=run_get_artist_info,
+    dag=dag
+)
+
+t1 >> t2 >> [t3, t4]
