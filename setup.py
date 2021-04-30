e23e7de7331dd7e3ba427c7c7d8c5c71b48fa278
flych3r <matheus.sampaio011@gmail.com>
2021-04-30 19:13:51 -0300

    :wrench: added stage to install package on deploy-serverless

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 setup(
     name='app',
     version='0.1.0',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     author='Matheus Xavier',
     description='Utility functions',
     packages=find_packages(),
